72ed9b2a8f33a6e6ad2da6a1779cdb66387571e2
Julie Rindone <julie.rindone@gmail.com>
2021-02-02 09:54:00 -0600

    Ch14_1 with notes_Finished

--- CodingEventsDemo/Controllers/EventsController.cs
+++ CodingEventsDemo/Controllers/EventsController.cs
@@ -5,8 +5,7 @@ using System.Threading.Tasks;
 using CodingEventsDemo.Data;
 using CodingEventsDemo.Models;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using CodingEventsDemo.ViewModels;
 
 namespace coding_events_practice.Controllers
 {
@@ -16,20 +15,37 @@ namespace coding_events_practice.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            ViewBag.events = EventData.GetAll();
+            //ViewBag.events = EventData.GetAll();   <-- getting rid of ViewBag; creating a list of Events
+            List<Event> events = new List<Event>(EventData.GetAll());
 
-            return View();
+            return View(events);  //<--passng the events list into the View as an argument to turn the view into a Razor view page based on an object.
         }
 
+
+        //[HttpGet]
         public IActionResult Add()
         {
-            return View();
+            //creat blank addeventviewmodel to associate with the Form in Add.cshtml
+            AddEventViewModel addEventViewModel = new AddEventViewModel();
+
+            return View(addEventViewModel);
         }
 
+        //"You feed it into the Get request empty, and it comes out full in the Post."
+
+        //we can rename this "Add" bc they have different argument signatures. The Get has 0, Post has 2.
+
+
         [HttpPost]
-        [Route("Events/Add")]
-        public IActionResult NewEvent(Event newEvent)
+        //now we take the filled adEventViewModel and use it to create a new Event object. ***we translate the info in the object into the info we want in the database.***
+        public IActionResult Add(AddEventViewModel addEventViewModel) 
         {
+            Event newEvent = new Event     //new curly brace syntax lets us assign properties here
+            {
+                Name = addEventViewModel.Name,
+                Description = addEventViewModel.Description
+            };
+
             EventData.Add(newEvent);
       
             return Redirect("/Events");
---
+++ CodingEventsDemo/ViewModels/AddEventViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEventsDemo.ViewModels
+{
+    public class AddEventViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        //has a default constructor, so we don't need to declare it.
+    }
+}
+
