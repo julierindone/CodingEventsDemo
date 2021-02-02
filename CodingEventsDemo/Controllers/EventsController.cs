7985f8ca7378a6160e8dd9a594976788d7318c32
Julie Rindone <julie.rindone@gmail.com>
2021-02-02 15:40:47 -0600

    Ch15onCh14Exercises

--- CodingEventsDemo/Controllers/EventsController.cs
+++ CodingEventsDemo/Controllers/EventsController.cs
@@ -7,7 +7,7 @@ using CodingEventsDemo.Models;
 using CodingEventsDemo.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
-// Learning different ways to enforce validation using attributes
+// We're creating a way to edit events.
 
 namespace coding_events_practice.Controllers
 {
@@ -30,34 +30,76 @@ namespace coding_events_practice.Controllers
 
         [HttpPost]
         public IActionResult Add(AddEventViewModel addEventViewModel)
-            //we'll use ModelState to see if the modelstate of addEventViewModel is valid with an if statement.
         {
             if (ModelState.IsValid)
-                //View model is valid:
+            //View model is valid:
             {
                 Event newEvent = new Event
                 {
                     Name = addEventViewModel.Name,
                     Description = addEventViewModel.Description,
-                    ContactEmail = addEventViewModel.ContactEmail    //Add email over here. Still not server-side validating.
+                    ContactEmail = addEventViewModel.ContactEmail    
                 };
 
                 EventData.Add(newEvent);
 
                 return Redirect("/Events");
-            }      //we don't need to use an else case bc the code in the id statement includes a return statement.
-
-            //new model is NOT valid! returning so they fill out the form again:
+            } 
 
             return View(addEventViewModel);
 
+        }
+        
+        //Get: /Events/Edit/{eventID}
+        [HttpGet]
+        [Route("/events/edit/{eventId}")]     //inputs the ID of the edit we need to do up into the Route.
 
+        public IActionResult Edit(int eventId)
+        {
+                //we need to make a editEventViewModel object with properties assigned from the Event associated with that specific eventId.
+            Event eventToEdit = EventData.GetById(eventId);         //this is holding the object we're editing. like declaring a variable. We have to do this because when we create the objkect below we have to pass it an Event.
 
+                //Use eventToEdit to populate values of editEventViewModel
+            EditEventViewModel editEventViewModel = new EditEventViewModel(eventToEdit);
 
-            
-      
+            return View(editEventViewModel);  //this displays the form to edit the event chosen in Event.cshtml
+        }
+
+                //Post /Events/Edit/
+        [HttpPost]
+
+        public IActionResult Edit(EditEventViewModel viewModel)
+        {
+            if (ModelState.IsValid)  //deal with validations! edit can only happen if fields pass validation.
+            {
+                //how does the post request know which id we're lookinh at?
+                //get the Event object from Event Data
+                // where does the eventId come from? it's stored in the viewModel.
+                Event eventToEdit = EventData.GetById(viewModel.Id);    //grabbing our eventToEdit from eventData by calling GetById (inside of viewModel.Id).
+
+                //Modify properties in eventToEdit using newest values stored in viewModel.
+                            //we need to have the ID here... we use a "hidden input" in the Edit.cshtml so it's a post-request parameter.
+                eventToEdit.Name = viewModel.Name;
+                eventToEdit.Description = viewModel.Description;
+                eventToEdit.ContactEmail = viewModel.ContactEmail;
+
+                return Redirect("/Events/");      //user sent back to /Events
+
+            }
+                //if not valid"
+            return View(viewModel);
             
         }
+
+
+
+
+
+
+
+
+
+
 
         public IActionResult Delete()
         {
---
+++ CodingEventsDemo/ViewModels/EditEventViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using CodingEventsDemo.Models;
+
+namespace CodingEventsDemo.ViewModels
+{
+    public class EditEventViewModel
+    {
+        
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]  ////with error message)
+
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 50 characters.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Description is required.")]
+        [StringLength(500, ErrorMessage = "Description is too long!")]
+        public string Description { get; set; }
+
+        [EmailAddress]
+        public string ContactEmail { get; set; }
+
+        public EditEventViewModel() { }  //we need an empty constructor so that viewModel (in controller) works. this is requirement of model binding.
+
+        public EditEventViewModel(Event evt)      //constructor that can take in an event object and create instance of EditEventViewModel
+        {
+            this.Id = evt.Id;
+            this.Name = evt.Name;
+            this.Description = evt.Description;
+            this.ContactEmail = evt.ContactEmail;
+        }
+
+    }
+}
