1eb234a914412a2eb44285014b07d499ee675002
Julie Rindone <julie.rindone@gmail.com>
2021-02-11 17:09:44 -0600

    Finished Ch 19 ex and started Studio

---
+++ CodingEventsDemo/Models/EventCategory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingEventsDemo.Models
+{
+    public class EventCategory
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public EventCategory()
+        {
+        }
+
+        public EventCategory(string name)
+        {
+            this.Name = name;
+        }
+
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Event @event &&
+                   Id == @event.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+    }
+}
---
+++ CodingEventsDemo/ViewModels/AddEventCategoryViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEventsDemo.ViewModels
+{
+    public class AddEventCategoryViewModel
+    {
+        [Required]
+        [StringLength(20, MinimumLength =3)]
+        public string Name { get; set; }
+    }
+}
