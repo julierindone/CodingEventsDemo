0d403cca1e0c40bd2ebfa219b8a7c4cdb708e21e
Julie Rindone <julie.rindone@gmail.com>
2021-02-12 13:20:59 -0600

    Finished Ch19 Studio

--- CodingEventsDemo/Controllers/EventCategoryController.cs
+++ CodingEventsDemo/Controllers/EventCategoryController.cs
@@ -1,9 +1,10 @@
-﻿using CodingEventsDemo.Data;
-using CodingEventsDemo.Models;
-using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using CodingEventsDemo.Data;
+using CodingEventsDemo.Models;
+using CodingEventsDemo.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
 namespace CodingEventsDemo.Controllers
@@ -22,6 +23,37 @@ namespace CodingEventsDemo.Controllers
         {
             List<EventCategory> eventCategories = context.EventCategories.ToList();
             return View(eventCategories);
+        }
+
+        [HttpGet]
+        [Route("/EventCategory/Create/")]
+        public IActionResult Create()
+        {
+            AddEventCategoryViewModel addEventCategories = new AddEventCategoryViewModel();
+
+            return View(addEventCategories);      //this might be wrong.... maybe EventCategoryView?
+        }
+
+        [HttpPost]
+        public IActionResult ProcessCreateEventCategoryForm(AddEventCategoryViewModel addEventCategories)    
+        {
+
+            if (ModelState.IsValid)
+            {
+
+                EventCategory newCategory = new EventCategory
+                {
+                    Name = addEventCategories.Name
+                };
+
+                context.EventCategories.Add(newCategory);
+                context.SaveChanges();
+
+                return Redirect("/EventCategory");
+            }
+
+            return View(addEventCategories);            //this might be wrong.... maybe EventCategoryView?
+
         }
     }
 }
--- CodingEventsDemo/ViewModels/AddEventCategoryViewModel.cs
+++ CodingEventsDemo/ViewModels/AddEventCategoryViewModel.cs
@@ -9,7 +9,7 @@ namespace CodingEventsDemo.ViewModels
     public class AddEventCategoryViewModel
     {
         [Required]
-        [StringLength(20, MinimumLength =3)]
+        [StringLength(20, MinimumLength = 3)]
         public string Name { get; set; }
     }
 }
